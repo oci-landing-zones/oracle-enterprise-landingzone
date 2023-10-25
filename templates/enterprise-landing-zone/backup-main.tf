9d6f729fbf84de6eb7e39e0791680908ec3c1e49
Yupei Yang <yupei.yang@oracle.com>
2023-10-25 13:31:24 -0700

    add spoke vcn for backup region

--- templates/enterprise-landing-zone/backup-main.tf
+++ templates/enterprise-landing-zone/backup-main.tf
@@ -2,17 +2,24 @@
 
 
 module "backup_prod_environment" {
-
-
+  source = "../elz-backup-environment"
+  workload_spoke_vcn_cidr = var.backup_prod_workload_cidr
+  workload_compartment_id = module.prod_environment.workload_compartment_id
 
   providers = {
     oci             = oci
     oci.backup_region = oci.backup_region
   }
 
+
 }
 
 module "backup_nonprod_environment" {
+  source = "../elz-backup-environment"
+
+  workload_spoke_vcn_cidr = var.backup_nonprod_workload_cidr
+  workload_compartment_id = module.nonprod_environment.workload_compartment_id
+
 
   providers = {
     oci             = oci
