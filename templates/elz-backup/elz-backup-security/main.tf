afc235194e53c8f98aee56c9c89bac8c52d1f836
Jagbir Singh <jag_techie@yahoo.com>
2023-11-29 15:17:20 -0800

    added license to terraform files

--- templates/elz-backup/elz-backup-security/main.tf
+++ templates/elz-backup/elz-backup-security/main.tf
@@ -1,3 +1,8 @@
+##########################################################################################################
+# Copyright (c) 2022,2023 Oracle and/or its affiliates, All rights reserved.                             #
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl. #
+##########################################################################################################
+
 locals {
   vault = {
     name = "${var.resource_label}-OCI-ELZ-VAL-${var.environment_prefix}-BACKUP-${local.region_key[0]}"
--- templates/elz-exadata-spoke/outputs.tf
+++ templates/elz-exadata-spoke/outputs.tf
@@ -1,3 +1,8 @@
+##########################################################################################################
+# Copyright (c) 2022,2023 Oracle and/or its affiliates, All rights reserved.                             #
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl. #
+##########################################################################################################
+
 output "spoke_lb_subnet_ocid" {
   value = module.workload_spoke_subnet.subnets[var.workload_private_spoke_subnet_lb_display_name]
 }
